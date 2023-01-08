dbc03716a6a1d3a8d6d1c5022a55def3395cfcc0
NadiaPeeya <nadiapeeya05@gmail.com>
2023-01-08 19:16:04 +0600

    updated

--- src/components/Home/BannerPage/BannerPage.js
+++ src/components/Home/BannerPage/BannerPage.js
@@ -4,9 +4,12 @@ import img2 from '../../../assets/Capture2.PNG';
 
 const BannerPage = () => {
     return (
-        <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginTop:'1rem'}}>
-            <img src={img} alt="" />
-           <img width="90%" src={img2} alt="" />
+        <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flext-start', marginTop:'6rem'}}>
+            <img style={{textAlign: 'left', marginRight: '25px'}} width="50%" src={img} alt="" />
+            <h1 style={{textAlign: 'left'}}>Maintain your healthy life 
+and keep your health data update always ... 
+</h1>
+           <img width="95%" src={img2} alt="" />
         </div>
     );
 };
